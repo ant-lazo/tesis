0885e86fba37a411481daf0dbd175ac16c0b886d
omarklazo <ant.14sag@gmail.com>
2021-04-13 13:55:43 -0500

    mi primer commit en las nubes para jemma

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccesHanler;
+import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private LoginSuccesHanler successHandler;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+		
+	@Autowired
+	private JpaUserDetailsService userDetailsService;
+	
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception
+	{
+	
+		builder.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder);
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/images/**","/listar**","/locale","/api/clientes/**").permitAll()
+		/*.antMatchers("/ver/**").hasAnyRole("USER")*/
+		/*.antMatchers("/uploads/**").hasAnyRole("USER")*/
+		/*.antMatchers("/form/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/eliminar/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/factura/**").hasAnyRole("ADMIN")*/
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+				.successHandler(successHandler)
+				.loginPage("/login")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403")
+		;
+	}
+	
+}
---
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.app.models.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.Producto;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
+	public Page<Cliente> findAll(Pageable pageable);
+	public void save(Cliente cliente);
+	public Cliente findOne(Long id);
+	public void delete(Long id);
+	public List<Producto> finByNombre(String term);
+	public void saveFactura (Factura factura);
+	public Producto findProductoById(Long id);
+	public Factura findFactruaById(Long id);
+	public void deleteFactura(Long id);
+	public Factura fetchFacturaByIdWithClienteWithItemFacturaWithProducto(Long id);
+	public Cliente fetchByIdWithFacturas(Long id);
+	
+}
